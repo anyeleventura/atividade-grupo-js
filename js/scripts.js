96c47a973d812bb93691d14e326c5f33a34ca483
anyeleventura <anyeleventuralima@gmail.com>
2023-02-28 19:49:22 -0300

    adiciona arquivos de demonstração de uso da array.values()

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// // cria um array para nomes
+// let nomes = [ 'Rebeca', 'Anyele', 'Taiane', 'Rosana' ];
+
+// // Aqui é chamada a função array.values()
+// let mostrar = nomes.values();
+
+// // Todos os elementos do array são chamados para a tela por uma estrutura de repetição
+// for (let elements of mostrar){
+//     document.write(elements + "<br>");
+// }
+
+
+
+// cria um array para nomes
+let nomes = [ 'Rebeca', 'Anyele', 'Taiane', 'Rosana' ];
+
+// Aqui é chamada a função array.values()
+let mostrar = nomes.values();
+
+// Todos os elementos do array são chamados para a tela nas próximas linhas
+document.write(mostrar.next().value + "<br>");
+document.write(mostrar.next().value + "<br>");
+document.write(mostrar.next().value + "<br>");
+document.write(mostrar.next().value + "<br>");
